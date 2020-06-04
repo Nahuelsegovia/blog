a7b2700bbd6b70908c7ad5f66d638d73d1e2447e
Nahuel Segovia <nahuelsegovia@MBP-de-Nahuel.fibertel.com.ar>
2020-06-03 23:40:06 -0300

    Heroku deployed version

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = 'us-cdbr-east-05.cleardb.net';
-$user = 'b345a843350680';
-$pass = 'd6a88fbe';
-$db = 'heroku_d381c2d1c045de7';
+$host = 'dee';
+$user = 'kdaleef';
+$pass = '009snndowd';
+$db = 'jekohdf';
 
 
 
