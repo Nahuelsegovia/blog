d8bd21f7f2159f2199d05ded65918fb572fe1dd1
Nahuel Segovia <nahuelsegovia@MBP-de-Nahuel.fibertel.com.ar>
2020-06-03 23:03:17 -0300

    Con mysql

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = 'localhost';
-$user = 'root';
-$pass = 'root';
-$db = 'blog';
+$host = 'us-cdbr-east-05.cleardb.net';
+$user = 'b345a843350680';
+$pass = 'd6a88fbe';
+$db = 'heroku_d381c2d1c045de7';
 
 
 
